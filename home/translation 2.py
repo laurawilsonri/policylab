9165e358ed5e5bc0912c990270179dfd09382b67
madams0013 <madelyn_adams@brown.edu>
2020-03-09 15:58:59 -0400

    updated spanish translations

---
+++ home/translation 2.py
@@ -0,0 +1,10 @@
+from .models import TransHomePage
+from modeltranslation.translator import TranslationOptions
+from modeltranslation.decorators import register
+
+
+@register(TransHomePage)
+class TransHomePageTR(TranslationOptions):
+    fields = (
+        'body',
+    ) 
---
+++ plab/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/maddyadams/Desktop/SocialChange/policylab/plab/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
